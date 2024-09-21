647e8075ab6641ac7fc13f2fbedfcba8e0f1f4b7
Luong Tran <tranthihienluong2003@gmail.com>
2024-09-21 23:31:57 +0700

    update get all targets

--- src/main/java/com/pbl6/bookstore/entity/Target.java
+++ src/main/java/com/pbl6/bookstore/entity/Target.java
@@ -1,5 +1,6 @@
 package com.pbl6.bookstore.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class Target {
     @JoinTable(name = "booktarget",
             joinColumns = @JoinColumn(name = "TargetID"),
             inverseJoinColumns = @JoinColumn(name = "BookID"))
+    @JsonIgnore
     List<Book> books;
 
     public Target() {
